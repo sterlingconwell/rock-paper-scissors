e6fa6bf6e9028a443c271906e59fe9d601a32668
Sterling Conwell <sterlingpconwell@gmail.com>
2025-03-02 18:04:31 -0800

    Add Score tracking to game

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+function getHumanChoice() {
+  const humanChoice = prompt("Enter your choice: ");
+  return humanChoice;
+}
+
+console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    return "It's a tie!";
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    return `You win! ${humanChoice} beats ${computerChoice}`;
+  } else {
+    computerScore++;
+    return `You lose! ${computerChoice} beats ${humanChoice}`;
+  }
+}
+
+console.log(`Score - Human: ${humanScore}, Computer: ${computerScore}`);
