f2df0b5e6e421bb0fc4d859074148158617f3139
Sterling Conwell <sterlingpconwell@gmail.com>
2025-03-02 17:31:15 -0800

    Add getComputerChoice function for rock paper scissors game

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+console.log(getComputerChoice());
